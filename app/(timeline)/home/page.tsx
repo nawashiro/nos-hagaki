74dbbf5b42f89a82da72ecd618ef44943b91eedf
nawashiro <hooochiba@gmail.com>
2024-01-18 22:53:45 +0900

    修正

--- app/(timeline)/home/page.tsx
+++ app/(timeline)/home/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
           べっ、別にあんたのためじゃないんだからねっ！
         </p>
         <div className="flex space-x-4">
-          <button className="h-10 px-4 py-2 border-2 border-neutral-200 rounded-[2rem] hover:bg-neutral-200">
+          <button className="px-4 py-2 border-2 border-neutral-200 rounded-[2rem] hover:bg-neutral-200">
             わかった
           </button>
           <Link
             href={"#"}
-            className="h-10 px-4 py-2 border-2 border-neutral-200 rounded-[2rem] hover:bg-neutral-200"
+            className="px-4 py-2 border-2 border-neutral-200 rounded-[2rem] hover:bg-neutral-200"
           >
             ヘルプを見る
           </Link>
