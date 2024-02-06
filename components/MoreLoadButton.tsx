3e0f1891cbf3094774ac606f190e2373d50925a5
nawashiro <hooochiba@gmail.com>
2024-02-06 17:57:36 +0900

    さらに読み込むを修正

--- components/MoreLoadButton.tsx
+++ components/MoreLoadButton.tsx
@@ -8,14 +8,14 @@ export default function MoreLoadButton({
 }: ButtonHTMLAttributes<HTMLButtonElement> & {
   valid: boolean;
 }) {
-  return (
-    valid && (
-      <SimpleButton
-        className="block mx-auto px-4 py-2 outline-2 outline outline-neutral-200 rounded-[2rem] hover:bg-neutral-200"
-        {...props}
-      >
-        さらに読み込む
-      </SimpleButton>
-    )
+  return valid ? (
+    <SimpleButton
+      className="block mx-auto px-4 py-2 outline-2 outline outline-neutral-200 rounded-[2rem] hover:bg-neutral-200"
+      {...props}
+    >
+      さらに読み込む
+    </SimpleButton>
+  ) : (
+    <p className="text-center py-2">がんばってます…</p>
   );
 }
--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -20,26 +20,22 @@ export default function Timeline({
 }) {
   return (
     <div className="space-y-8">
-      {moreLoadButtonValid ? (
-        <div className="space-y-4">
-          {Array.from(timeline.eventList)
-            .sort((a, b) => {
-              const dateA = a.created_at || 0;
-              const dateB = b.created_at || 0;
-              return dateB - dateA;
-            })
-            .map((event, index) => (
-              <EventCard
-                event={event}
-                key={index}
-                regions={regions}
-                profiles={profiles}
-              />
-            ))}
-        </div>
-      ) : (
-        <p className="text-center py-2">がんばってます…</p>
-      )}
+      <div className="space-y-4">
+        {Array.from(timeline.eventList)
+          .sort((a, b) => {
+            const dateA = a.created_at || 0;
+            const dateB = b.created_at || 0;
+            return dateB - dateA;
+          })
+          .map((event, index) => (
+            <EventCard
+              event={event}
+              key={index}
+              regions={regions}
+              profiles={profiles}
+            />
+          ))}
+      </div>
       <MoreLoadButton
         valid={moreLoadButtonValid && timeline.eventList.size != 0}
         onClick={getMoreEvent}
