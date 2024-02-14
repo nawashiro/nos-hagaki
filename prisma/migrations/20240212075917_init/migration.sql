644bd22289bdbd81a1f6e01a0e9c38cf5399110b
nawashiro <hooochiba@gmail.com>
2024-02-14 19:25:07 +0900

    db更新

--- prisma/migrations/20240212075917_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `ip` to the `Posts` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Posts" ADD COLUMN     "ip" TEXT NOT NULL;
