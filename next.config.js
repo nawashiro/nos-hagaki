8aacaea3e1bffd6ca99b2ffeb0c366d5d7d7215a
nawashiro <hooochiba@gmail.com>
2024-02-19 18:43:46 +0900

    セキュリティポリシーを追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    headers: [
+      {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+      },
+      {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+      },
+      {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+      },
+      {
+        key: "Content-Security-Policy",
+        value: cspHeader.replace(/\n/g, ""),
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
