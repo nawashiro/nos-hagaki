a9fa85ac7776cc7f555ec26490c76e2c2f93e9e6
K.O <whitecat6142+git@gmail.com>
2024-03-06 21:35:36 +0900

    Update getRegions.ts

--- src/getRegions.ts
+++ src/getRegions.ts
@@ -86,7 +86,7 @@ export async function getRegions(pubkeys: string[]) {
     const region: Region = (() => {
       while (true) {
         const longitude = rng() * 360 - 180; // 経度を-180から180の間でランダムに選ぶ
-        const latitude = Math.acos(2 * rng() - 1) * (180 / Math.PI) - 90; // 緯度を-90から90の間でランダムに選ぶ
+        const latitude = Math.asin(2 * rng() - 1) * (180 / Math.PI); // 緯度を-90から90の間でランダムに選ぶ
 
         const countryName = getCountryName(latitude, longitude, features);
 
